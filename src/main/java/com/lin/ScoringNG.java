2b60720f260c40c16ee9605ad056879f7e176fe5
LNG00 <rita.lr007@gmail.com>
2022-03-04 12:56:41 +0800

    ScoringNG

---
+++ src/main/java/com/lin/ScoringNG.java
@@ -0,0 +1,12 @@
+package com.lin;
+
+public class ScoringNG {
+    public static void main(String[] args) {
+        int[] chinese = {90, 85, 80, 92, 75 };
+        int[] engish = {80, 85, 90, 78, 95 };
+        String[]name = {"Join", "Joanna", "May ", "Nana", "Selina"};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name[i]+"\t"+chinese[i]+"\t"+engish[i]+"\t"+(chinese[i]+engish[i])/2);
+        }
+    }
+}
